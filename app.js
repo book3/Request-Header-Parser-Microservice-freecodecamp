2913e72d10d8793635a79a05182c49fc97ffbe92
book3 <y.karagulov@gmail.com>
2018-04-12 21:33:23 -0400

    Request-Header-Parser-Micros ervice-freecodecamp

--- app.js
+++ app.js
@@ -7,39 +7,14 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.set('view engine','ejs');
 
 
-app.get('/',function(req,res){
-  res.render('index');
+app.get('/api/woami',urlencodedParser,function(req,res){
+
+  var i={
+    ip: req.socket.remoteAddress,
+    language: req.headers['accept-language'],
+    userAgent: req.headers['user-agent']
+  };
+  res.json(i);
 });
-
-app.get('/:name',function(req,res){
-  if(!isNaN(req.params.name)){
-    var d = new Date(Number(req.params.name)*1000+86400000);
-    var d = d.toDateString();
-    var data ={
-      unix: req.params.name,
-      natural: d
-    };
-      res.json(data);
-  }
-  else{
-    var nat= Date.parse(req.params.name);
-    if(!isNaN(nat)){
-      var unix=nat/1000;
-      var data ={
-        unix: unix,
-        natural: req.params.name
-      };
-      res.json(data);
-    }else {
-      var data ={
-        unix: null,
-        natural: null
-      };
-      res.json(data);
-    }
-  }
-
-});
-
 
 app.listen(3000);
